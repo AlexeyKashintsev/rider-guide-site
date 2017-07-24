3630841b5dbb500c4a7cbf893feddaab811eed98
ils_hs <shishin98@gmail.com>
2017-07-24 15:14:47 +0300

    buttons

---
+++ assets/js/animbutton.js
@@ -0,0 +1,21 @@
+/*$('document').ready(function($) {
+    
+var btn = $("button");
+    
+change = function(but) {
+    if ( but.style.width === "200px" ){
+            but.style.width = '400px';
+        }
+    else{
+        but.style.width = '200px';
+    }    
+}
+    
+btn.click(function(){
+     var c = this;
+     setInterval(function(с){
+         change(c);
+     }, 100)
+ });*/
+    
+})
