40a549cf9fcd0e3665ceac379d834a64e58f0cee
George Vikhrev <georgevikhrev@gmail.com>
2017-07-25 20:35:28 +0300

    sport choosing page

--- assets/js/dynamic-add.js
+++ assets/js/dynamic-add.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
     
-    sportNames = ["skate", "bmx", "mtb", "shishikhin", "peetooh", "very", "gay", "ochen"]
+    sportNames = ["skate", "bmx", "mtb", "Longboard", "Motorcycle", "Car", "Quadrocycle" ];
+//    sportNames = ["Snowboard", "Ski", "Freeski", "SnowScoot", "SnowMobile"];
     
     sportNames.forEach(function(sportName, i){
         addDynamicExtraField(sportName);            
@@ -8,11 +9,18 @@ jQuery(document).ready(function($) {
     
     function addDynamicExtraField(sportName) {
         
-        var div = $('#buttoncontainer')
+        var div = $('#buttonscontainer');
+        var btncntr = $('<div/>', {
+            class: 'btn-container'
+        }).appendTo(div);
         
-        $('<button/>', {
-            class: 'btn-sport',
+        var btn = $('<button/>', {
+            class: 'btn-'+ sportName +' btn-sport',
             value: sportName
-        }).html(sportName).appendTo(div)
+        }).appendTo(btncntr);
+        
+        $('<span/>', {
+            class: 'txt-inside-btn'
+        }).html(sportName).appendTo(btn);
     }
 })
